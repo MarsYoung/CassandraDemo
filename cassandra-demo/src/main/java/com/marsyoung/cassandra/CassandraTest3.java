90f1ec49fe5171323c9e8bd029996d003adc72be
马志宇(无线运营中心) <zhiyuma@sohu-inc.com>
2015-07-27 18:49:06 +0800

    多线程测试代码更新

--- cassandra-demo/src/main/java/com/marsyoung/cassandra/CassandraTest3.java
+++ cassandra-demo/src/main/java/com/marsyoung/cassandra/CassandraTest3.java
@@ -1,5 +1,6 @@
 package com.marsyoung.cassandra;
 
+import java.util.Random;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -8,91 +9,94 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class CassandraTest3 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
 				new String[] { "spring-data-cassandra.xml" });
+		UserTokenDao utDao = context.getBean(UserTokenDao.class);
 		UserTokenDao2 utDao2 = context.getBean(UserTokenDao2.class);
-		
+
 		ExecutorService executor = Executors.newFixedThreadPool(100);
 		CountDownLatch latch = new CountDownLatch(100);
 		for (int i = 0; i < 100; i++) {
-			if (i < 100) {
-				executor.submit(new WriteCassandra(latch, utDao2));
+			if (i < 95) {
+				executor.submit(new WriteCassandra(latch, utDao));
 			} else {
 				executor.submit(new ReadCassandra(latch, utDao2));
 			}
 		}
+		latch.await();
 	}
 
 	static class WriteCassandra implements Runnable {
 		CountDownLatch latch;
-		UserTokenDao2 utDao2;
+		UserTokenDao utDao;
 
-		public WriteCassandra(CountDownLatch latch, UserTokenDao2 utDao2) {
+		public WriteCassandra(CountDownLatch latch, UserTokenDao utDao) {
 			this.latch = latch;
-			this.utDao2 = utDao2;
+			this.utDao = utDao;
 		}
 
 		public void run() {
 			latch.countDown();
+			Random random = new Random(1000);
+			int i = 0;
+			long start = System.currentTimeMillis();
 			while (true) {
-				for (int i = 0; i < 10000; i++) {
-					UserToken ut=createWrite(i);
-					long start = System.currentTimeMillis();
-					utDao2.save(ut);
-					if (i % 10000 == 0) {
-						long end = System.currentTimeMillis();
-						long cost = end - start;
-						System.out.println("write 10000 cost : " + cost + " ms");
-						i = 0;
-					}
+				utDao.save(createWrite(random));
+				if (++i % 10000 == 0) {
+					long end = System.currentTimeMillis();
+					long cost = end - start;
+					System.out.println("write 10000 cost : " + cost + " ms");
+					start = System.currentTimeMillis();
 				}
-			}
 
+			}
 		}
 	}
-	
-	static UserToken createWrite(Integer i){
-		String s = new Integer(i).toString();
-		long l = (long) i;
-		String passport=s+s+"@sohu.com";
-		long appId=l;
-		String gid= s+s;
-		long plat=l+l;
-		UserToken ut = new UserToken(passport, appId, gid,plat, s+s+s+s, l+l+l);
+
+	static final long appId = 1;
+	static final String gid = "cyrilTestgid---daeaete";
+	static final long plat = 1;
+	static final String userToken = "cyrilTestuserToken--daedae";
+
+	static UserToken createWrite(Random random) {
+		int i = random.nextInt(1000);
+		String passport = i + "@sohu.com";
+		UserToken ut = new UserToken(passport, appId, gid, plat, userToken,
+				random.nextInt(1000));
 		return ut;
+	}
+
+	static String createRead(Random random) {
+		int i = random.nextInt(1000);
+		String passport = i + "@sohu.com";
+		return passport;
 	}
 
 	static class ReadCassandra implements Runnable {
 		CountDownLatch latch;
-		UserTokenDao2 utDao2;
+		UserTokenDao2 utDao;
 
-		public ReadCassandra(CountDownLatch latch, UserTokenDao2 utDao2) {
+		public ReadCassandra(CountDownLatch latch, UserTokenDao2 utDao) {
 			this.latch = latch;
-			this.utDao2 = utDao2;
+			this.utDao = utDao;
 		}
 
 		public void run() {
 			latch.countDown();
+			Random random = new Random(1000);
+			int i = 0;
+			long start = System.currentTimeMillis();
 			while (true) {
-				for (int i = 0; i < 10000; i++) {
-					String s = new Integer(i).toString();
-					long l = (long) i;
-					String passport=s+s+"@sohu.com";
-					long appId=l;
-					String gid= s+s;
-					long plat=l+l;
-					long start = System.currentTimeMillis();
-					utDao2.findUserTokenByPrimaryKeyColumn(passport, appId, gid,
-							plat);
-					if (i % 10000 == 0) {
-						long end = System.currentTimeMillis();
-						long cost = end - start;
-						System.out
-								.println("write 10000 cost : " + cost + " ms");
-						i = 0;
-					}
+				
+				utDao.findUserTokenByPrimaryKeyColumn(createRead(random),
+						appId, gid, plat);
+				if (++i % 10000 == 0) {
+					long end = System.currentTimeMillis();
+					long cost = end - start;
+					System.out.println("read 10000 cost : " + cost + " ms");
+					start = System.currentTimeMillis();
 				}
 
 			}
